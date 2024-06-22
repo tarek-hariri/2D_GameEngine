4850444922ec0caefac37536111c0c1313fa2371
Tarek Hariri <haririt@uci.edu>
2024-06-21 19:38:08 -0700

    Added DISPLAY_HEIGHT and DISPLAY_WEIGHT

--- SDLStuff/Settings.h
+++ SDLStuff/Settings.h
@@ -4,3 +4,5 @@ constexpr int TILE_SIZE = 16;
 constexpr float SCALE = 1;
 constexpr float SCALED_TILE_SIZE = TILE_SIZE * SCALE;
 constexpr int MAP_SIZE = 10;
+constexpr int DISPLAY_WIDTH = 768;
+constexpr int DISPLAY_HEIGHT = 640;
